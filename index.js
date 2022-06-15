e7030674fbf4bb6a7a8570b51c9e510c5578f894
dipantan <sanju.mahinagar@gmail.com>
2022-06-15 19:26:23 +0530

    changed port

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import router from './api/router';
 const app = express();
 app.use(json());
 app.use('/', router);
-app.listen(80, function () {
+app.listen(3000, function () {
     console.log('listening on port 80');
 });
