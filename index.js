73cbcce1378dc5d79f040894d8999a3bf620c868
dipantan <sanju.mahinagar@gmail.com>
2022-06-16 00:00:45 +0530

    updated

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express, { json } from 'express';
-import * as router from './api/router.js';
+import router from './api/router.js';
 
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(json());
-// app.use('/', router);
+app.use('/', router);
 app.listen(port, function () {
     // console.log(router);
     console.log('listening on port ' + port);
