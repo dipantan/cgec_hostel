fab8b74301319e58d7947d69a52b92c6160d1237
dipantan <sanju.mahinagar@gmail.com>
2022-06-27 13:27:45 +0530

    added cors package

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express, { json } from 'express';
 import router from './api/router.js';
+import cors from 'cors'
 
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(json());
+app.use(cors)
 app.use('/', router);
 app.listen(port, function () {
     // console.log(router);
