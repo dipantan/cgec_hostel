e490f4cac0c53db732f4a221cf6f0fb1bc1b6765
dipantan <sanju.mahinagar@gmail.com>
2022-09-01 18:10:01 +0530

    fixed

--- service/connection.js
+++ service/connection.js
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
 import { createConnection } from "mysql2";
-// const MYSQLHOST = 'containers-us-west-34.railway.app'
-// const MYSQLUSER = 'root'
-// const MYSQLDATABASE = 'railway'
-// const MYSQLPASSWORD = 'iU4OSNlbhmjuw1FEHCYn'
-// const MYSQLPORT = 6462
-const MYSQLHOST = "localhost";
+
+const MYSQLHOST = "containers-us-west-34.railway.app";
 const MYSQLUSER = "root";
-const MYSQLDATABASE = "cgec_hostel";
-const MYSQLPASSWORD = "R}~&?4OS[k!zrpB";
-const MYSQLPORT = 3306;
-=======
-import { createConnection } from 'mysql2'
-const MYSQLHOST = 'containers-us-west-34.railway.app'
-const MYSQLUSER = 'root'
-const MYSQLDATABASE = 'railway'
-const MYSQLPASSWORD = 'lCFxSkVAMQBJjPDVrZUC'
-const MYSQLPORT = 6462
->>>>>>> 1075e96e20239d028f420a70d921f374f0cb651c
+const MYSQLDATABASE = "railway";
+const MYSQLPASSWORD = "lCFxSkVAMQBJjPDVrZUC";
+const MYSQLPORT = 6462;
+
 const connection = createConnection({
   host: MYSQLHOST,
   user: MYSQLUSER,
@@ -34,8 +22,4 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
-<<<<<<< HEAD
 export default connection;
-=======
-export default connection
->>>>>>> 1075e96e20239d028f420a70d921f374f0cb651c
