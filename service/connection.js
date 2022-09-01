d8b3f22786c996241e3c7ad91fa9715fe0ca9fce
dipantan <sanju.mahinagar@gmail.com>
2022-09-01 10:22:10 +0530

    added

--- service/connection.js
+++ service/connection.js
@@ -1,23 +1,28 @@
-import { createConnection } from 'mysql2'
-const MYSQLHOST = 'containers-us-west-34.railway.app'
-const MYSQLUSER = 'root'
-const MYSQLDATABASE = 'railway'
-const MYSQLPASSWORD = 'iU4OSNlbhmjuw1FEHCYn'
-const MYSQLPORT = 6462
+import { createConnection } from "mysql2";
+// const MYSQLHOST = 'containers-us-west-34.railway.app'
+// const MYSQLUSER = 'root'
+// const MYSQLDATABASE = 'railway'
+// const MYSQLPASSWORD = 'iU4OSNlbhmjuw1FEHCYn'
+// const MYSQLPORT = 6462
+const MYSQLHOST = "localhost";
+const MYSQLUSER = "root";
+const MYSQLDATABASE = "cgec_hostel";
+const MYSQLPASSWORD = "R}~&?4OS[k!zrpB";
+const MYSQLPORT = 3306;
 const connection = createConnection({
-    host: MYSQLHOST,
-    user: MYSQLUSER,
-    database: MYSQLDATABASE,
-    password: MYSQLPASSWORD,
-    port: MYSQLPORT
-})
+  host: MYSQLHOST,
+  user: MYSQLUSER,
+  database: MYSQLDATABASE,
+  password: MYSQLPASSWORD,
+  port: MYSQLPORT,
+});
 
 connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack)
-        return
-    }
-    console.log('connected as id ' + connection.threadId)
-})
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-export default connection
+export default connection;
--- service/service.js
+++ service/service.js
@@ -1,112 +1,141 @@
-import connection from './connection.js';
+import connection from "./connection.js";
 
 // function to register students
 export function insertStudents(data, callback) {
-    connection.query(
-        "insert into students (email,password,mobile,department,semester,name,roll) values (?,?,?,?,?,?,?)",
-        [data.email, data.password, data.mobile, data.department, data.semester, data.name, data.roll],
-        function (err, result) {
-            if (err) {
-                return callback(err, null);
-            }
-            return callback(null, result);
-        }
-    )
+  connection.query(
+    "insert into students (email,password,mobile,department,semester,name,roll) values (?,?,?,?,?,?,?)",
+    [
+      data.email,
+      data.password,
+      data.mobile,
+      data.department,
+      data.semester,
+      data.name,
+      data.roll,
+    ],
+    function (err, result) {
+      if (err) {
+        return callback(err, null);
+      }
+      return callback(null, result);
+    }
+  );
 }
 
 // function to update students
 export function updateStudents(data, callback) {
-    connection.query(
-        "update students set password=?,mobile=?,department=?,semester=?,name=?,roll=? where email=?",
-        [data.password, data.mobile, data.department, data.semester, data.name, data.roll, data.email],
-        function (err, result) {
-            if (err) {
-                return callback(err, null);
-            }
-            return callback(null, result);
-        }
-    )
+  connection.query(
+    "update students set password=?,mobile=?,department=?,semester=?,name=?,roll=? where email=?",
+    [
+      data.password,
+      data.mobile,
+      data.department,
+      data.semester,
+      data.name,
+      data.roll,
+      data.email,
+    ],
+    function (err, result) {
+      if (err) {
+        return callback(err, null);
+      }
+      return callback(null, result);
+    }
+  );
 }
 
 // function to delete students
 export function deleteStudents(data, callback) {
-    connection.query(
-        "delete from students where email=?",
-        [data.email],
-        function (err, result) {
-            if (err) {
-                return callback(err, null);
-            }
-            return callback(null, result);
-        }
-    )
+  connection.query(
+    "delete from students where email=?",
+    [data.email],
+    function (err, result) {
+      if (err) {
+        return callback(err, null);
+      }
+      return callback(null, result);
+    }
+  );
 }
 
 // function to get students
 export function getStudents(data, callback) {
-    console.log(data);
-    connection.query(
-        "select * from students where email=? and password=?",
-        [data.email, data.password],
-        function (err, result) {
-            if (err) {
-                return callback(err, null);
-            }
-            return callback(null, result);
-        }
-    )
+  console.log(data);
+  connection.query(
+    "select * from students where email=? and password=?",
+    [data.email, data.password],
+    function (err, result) {
+      if (err) {
+        return callback(err, null);
+      }
+      return callback(null, result);
+    }
+  );
 }
 
 // function to insert room change requests
 export function insertRoomChange(data, callback) {
-    connection.query("select * from room_change_request where email = ? and status = 'pending'",
-        [data.email],
-        function (err, result) {
-            if (err) {
-                return callback(err, null)
-            } else {
-                if (result.length > 0) {
-                    // previous request not completed
-                    return callback("error", null)
-                } else {
-                    // accept new request
-                    var date = new Date().toLocaleDateString()  //  mm/dd/yyyy
-                    connection.query(
-                        "insert into room_change_request (from_room,to_room,reason,date,email) values (?,?,?,?,?)",
-                        [data.from_room, data.to_room, data.reason, date, data.email],
-                        function (err, result) {
-                            if (err) {
-                                return callback(err, null)
-                            }
-                            return callback(null, result)
-                        }
-                    )
-                }
+  connection.query(
+    "select * from room_change_request where email = ? and status = 'pending'",
+    [data.email],
+    function (err, result) {
+      if (err) {
+        return callback(err, null);
+      } else {
+        if (result.length > 0) {
+          // previous request not completed
+          return callback("error", null);
+        } else {
+          // accept new request
+          var date = new Date().toLocaleDateString(); //  mm/dd/yyyy
+          connection.query(
+            "insert into room_change_request (from_room,to_room,reason,date,email,status) values (?,?,?,?,?,?)",
+            [
+              data.from_room,
+              data.to_room,
+              data.reason,
+              date,
+              data.email,
+              "pending",
+            ],
+            function (err, result) {
+              if (err) {
+                return callback(err, null);
+              }
+              return callback(null, result);
             }
-        })
+          );
+        }
+      }
+    }
+  );
 }
 
 // function to insert complain
 export function insertComplain(data, callback) {
-    connection.query("insert into complain (description,email,title) values (?,?,?)",
-        [data.description, data.email, data.title],
-        function (err, result) {
-            if (err) {
-                return callback(err, null)
-            }
-            return callback(null, result)
-        })
+  connection.query(
+    "insert into complain (description,email,title) values (?,?,?)",
+    [data.description, data.email, data.title],
+    function (err, result) {
+      if (err) {
+        return callback(err, null);
+      }
+      return callback(null, result);
+    }
+  );
 }
 
 // function to start/stop meal
 export function chnageMeal(data, callback) {
-    connection.query("insert into meal (`email`,`preference`,`from`,`meal_type`,`to`) values (?,?,?,?,?)",
-        [data.email, data.preference, data.from, data.meal_type, data.to],
-        function (err, result) {
-            if (err) {
-                return callback(err, null)
-            } else {
-                return callback(null, result)
-            }
-        })
+  connection.query(
+    "insert into meal (`email`,`preference`,`from`,`meal_type`,`to`) values (?,?,?,?,?)",
+    [data.email, data.preference, data.from, data.meal_type, data.to],
+    function (err, result) {
+      if (err) {
+        return callback(err, null);
+      } else {
+        return callback(null, result);
+      }
+    }
+  );
 }
